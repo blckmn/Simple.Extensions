57500cd637d2d27fb06291d9e500f5f91fa5d67d
blckmn <blackman@xtra.com.au>
2019-06-22 22:32:14 +1000

    Initial commit

---
+++ Simple.Extensions.Data/DataExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+
+namespace Simple.Extensions.Data
+{
+    public static class DataExtensions
+    {
+        public static T GetValue<T>(this IDataReader reader, string fieldName)
+        {
+            if (fieldName == null)
+            {
+                throw new ArgumentNullException(nameof(fieldName));
+            }
+
+            try
+            {
+                int index = reader.GetOrdinal(fieldName);
+
+                var result = reader.GetValue(index);
+
+                if (result == DBNull.Value)
+                {
+                    return default;
+                }
+
+                return (T)result;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new Exception($"Field not found {fieldName}");
+            }
+        }
+
+        public static bool FieldExists(this IDataReader reader, string fieldName)
+        {
+            try
+            {
+                for (var pos = 0; pos < reader.FieldCount; pos++)
+                {
+                    if (reader.GetName(pos).Equals(fieldName, StringComparison.CurrentCultureIgnoreCase))
+
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Simple.Extensions.String/StringExtension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Simple.Extensions.String
+{
+    public static class StringExtension
+    {
+        public static bool IsAny(this string value, params string[] list)
+        {
+            if (value == null || list == null)
+            {
+                return false;
+            }
+
+            if (list.Contains(value, StringComparer.CurrentCultureIgnoreCase))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool EqualsCi(this string value, string compare)
+        {
+            if (value == null || compare == null)
+            {
+                return false;
+            }
+
+            if (value.Equals(compare, StringComparison.CurrentCultureIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static string[] ComplimentarySet(this string[] value, params string[] list)
+        {
+            if (value == null || list == null)
+            {
+                return new string[0];
+            }
+
+            var result = new List<string>();
+            foreach(var item in list)
+            {
+                if (item.IsAny(value))
+                {
+                    result.Add(item);
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
