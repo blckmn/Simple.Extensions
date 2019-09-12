a1a43d96620f4b4053427e6f32cbdbda8bde9615
Blackman <jason.blackman@carsales.com.au>
2019-09-13 09:06:42 +1000

    Adding overload for passing default value if DBNull.

--- Simple.Extensions.Data/DataExtensions.cs
+++ Simple.Extensions.Data/DataExtensions.cs
@@ -5,6 +5,35 @@ namespace Simple.Extensions.Data
 {
     public static class DataExtensions
     {
+        public static T GetValue<T>(this IDataReader reader, string fieldName, T defaultValue)
+        {
+            if (fieldName == null)
+            {
+                throw new ArgumentNullException(nameof(fieldName));
+            }
+
+            try
+            {
+                var index = reader.GetOrdinal(fieldName);
+
+                var result = reader.GetValue(index);
+                if (result == DBNull.Value)
+                {
+                    return defaultValue;
+                }
+
+                return (T)result;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new Exception($"Field not found: {fieldName}");
+            }
+            catch (InvalidCastException ex)
+            {
+                throw new InvalidCastException($"Invalid cast: {typeof(T).Name}, for field: {fieldName} -> {ex.Message}", ex);
+            }
+        }
+
         public static T GetValue<T>(this IDataReader reader, string fieldName)
         {
             if (fieldName == null)
@@ -22,7 +51,7 @@ namespace Simple.Extensions.Data
                     return default;
                 }
 
-                return (T) result;
+                return (T)result;
             }
             catch (IndexOutOfRangeException)
             {
