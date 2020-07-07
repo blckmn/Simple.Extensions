82bb4e123edb32d485199b8b42993af0ce70fc72
blckmn <blackman@xtra.com.au>
2020-07-07 15:57:21 +1000

    Adding CollectionExtensions

---
+++ Simple.Extensions.Collection/CollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Simple.Extensions.Collection
+{
+    public static class CollectionExtensions
+    {
+        public static void Each<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            foreach (var t in enumerable)
+            {
+                action.Invoke(t);
+            }
+        }
+
+        public static async Task EachAsync<T>(this IEnumerable<T> enumerable, Func<T, Task> action, int maxThreads, CancellationToken token)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            var actionBlock = new ActionBlock<T>(
+                async i => { await action.Invoke(i); },
+                new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = maxThreads, 
+                    CancellationToken = token
+                }
+            );
+
+            foreach (var i in enumerable)
+            {
+                actionBlock.Post(i);
+            }
+            actionBlock.Complete();
+
+            await actionBlock.Completion;
+        } 
+    }
+}
